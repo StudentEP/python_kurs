05893c79ab119ca7e716acc3f05a757c341c6a5b
EPILZAK <edytapilzak40@gmail.com>
2023-02-19 13:36:23 +0100

    krok 6

--- main.py
+++ main.py
@@ -24,46 +24,60 @@ class Segment:
     def svg(self):
         return f'<line x1="{self.p1.x}" y1="{self.p1.y}" x2="{self.p2.x}" y2"{self.p2.y}" style="stroke:black" />'
 
+
 class Polygon:
     def __init__(self):
-        self.vertices=[]
+        self.vertices = []
 
-    def add(self,vertex):
+    def add(self, vertex):
         self.vertices.append(vertex)
 
     def __str__(self) -> str:
-        vertices_str=""
-        for vertex in self.vertices:
-            vertices_str+=str(vertex) + ", "
-        return f"Polygon({vertices_str[:-2]})"
-    def svg(self):
-        #<polygon points = "200, 10 250,190 160,210" / >
         vertices_str = ""
         for vertex in self.vertices:
-            vertices_str += f'{vertex.x,},{vertex.y} '
+            vertices_str += str(vertex) + ", "
+        return f"Polygon({vertices_str[:-2]})"
+
+    def svg(self):
+        # <polygon points = "200, 10 250,190 160,210" / >
+        vertices_str = ""
+        for vertex in self.vertices:
+            vertices_str += f'{vertex.x},{vertex.y} '
         return f'<polygon points = "{vertices_str[:-1]}"/>'
 
     def area(self):
         result = 0
         for i in range(len(self.vertices)):
             pa = self.vertices[i]
-            pn = self.vertices[(i+1)%len(self.vertices)]
-            det = (pa.x*pn.y) - (pa.y*pn.x)
+            pn = self.vertices[(i + 1) % len(self.vertices)]
+            det = (pa.x * pn.y) - (pa.y * pn.x)
             result += det
-            return abs(result/2)
-   def main():
-     p = Point(300, 0)
-     q = Point(0, 400)
+        return abs(result / 2)
 
-     polygon = Polygon()
-     polygon.add(p)
-     polygon.add(q)
-     polygon.add(Point(300,400))
+    @staticmethod
+    def regular_pentagon(radius):
+        polygon = Polygon()
+        for i in range(5):
+            x = radius * math.cos(math.radians(72 * i))
+            y = radius * math.sin(math.radians(72 * i))
+            polygon.add(Point(x, y))
+        return polygon
 
-     # print(polygon.svg())
 
-      print(polygon.area())
+def main():
+    p = Point(300, 0)
+    q = Point(0, 400)
 
+    polygon = Polygon()
+    polygon.add(p)
+    polygon.add(q)
+    polygon.add(Point(300, 400))
+
+    # print(polygon.svg())
+    # print(polygon.area())
+
+    pentagon = Polygon.regular_pentagon(150)
+    print(pentagon.svg())
 
 
 main()
