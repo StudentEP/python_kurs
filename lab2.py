a56c0354ae017b00330ca813faa557573a45ab86
EPILZAK <edytapilzak40@gmail.com>
2023-03-05 13:04:55 +0100

    krok5

--- lab2.py
+++ lab2.py
@@ -31,6 +31,33 @@ def solve_Josephus(people, step):
 def sort_by_age(people):
     people.sort(key=lambda person:person.date_of_birth)
 
+def merge_sort(arr):
+    if len(arr)>1:
+        mid=len(arr)//2
+        left=arr[:mid]
+        right=arr[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i=j=k=0
+        while i<len(left) and j<len(right):
+            if left[i]<right[j]:
+                arr[k]=left[i]
+                i+=1
+
+            else:
+                arr[k]=right[j]
+                j+=1
+            k+=1
+        while i<len(left):
+            arr[k]=left[i]
+            i+=1
+            k+=1
+        while j<len(right):
+            arr[k]=right[j]
+            j+=1
+            k+=1
+
+
 
 def main():
 
@@ -38,6 +65,9 @@ def main():
     sort_by_age(people)
     for person in people:
         print(person)
+    arr=[2, 7, 19, -5, -26]
+    merge_sort(arr)
+    print(arr)
 
 
 
