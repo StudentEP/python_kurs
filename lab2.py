82c3e1cc0b4f4027ec6fb42e14beb8e9a2e69afa
EPILZAK <edytapilzak40@gmail.com>
2023-03-05 13:51:21 +0100

    krok7

--- lab2.py
+++ lab2.py
@@ -57,14 +57,20 @@ def merge_sort(arr, key):
             j+=1
             k+=1
 
-def filter_by_last_name(people, substr):
+def filter_by_last_name_(people, substr):
    return list(filter(lambda person:substr.lower() in person.last_name.lower(), people))
+
+def filter_by_last_name(people, substr):
+    result=set()
+    for word in substr.split(" "):
+        result.update(filter_by_last_name_(people, word))
+    return list(result)
 
 def main():
 
     people = people_from_csv("lista")
     sort_by_age(people)
-    people=filter_by_last_name(people, "nn")
+    people=filter_by_last_name(people, "ko no")
     for person in people:
         print(person)
     print(people)
