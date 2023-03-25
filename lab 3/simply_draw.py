051ccdcdf8460e8b524b68d3121871a5d7081c55
EPILZAK <edytapilzak40@gmail.com>
2023-03-25 14:36:35 +0100

    zad 1 kr 5

---
+++ lab 3/simply_draw.py
@@ -0,0 +1,42 @@
+import turtle
+
+def draw_rect():
+    screen = turtle.Screen()
+    t = turtle.Turtle()
+
+    t.speed(1)
+
+    t.forward(50)
+    t.right(90)
+    t.forward(50)
+    t.right(90)
+    t.forward(50)
+    t.right(90)
+    t.forward(50)
+
+    screen.exitonclick()
+
+def draw_star():
+    screen = turtle.Screen()
+    t = turtle.Turtle()
+
+    t.speed(1)
+
+    for _ in range(5):
+        t.forward(100)
+        t.right(144)
+
+    screen.exitonclick()
+
+
+def draw_reg_poly(n):
+    screen = turtle.Screen()
+    t = turtle.Turtle()
+
+    t.speed(1)
+
+    for _ in range(n):
+        t.forward(50)
+        t.left(360 / n)
+
+    screen.exitonclick()
