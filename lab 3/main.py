7fff179662528a8600d26a52500c51e0f47a1582
EPILZAK <edytapilzak40@gmail.com>
2023-03-26 11:28:20 +0200

    zad 1 kr 2

--- lab 3/main.py
+++ lab 3/main.py
@@ -2,7 +2,7 @@
 import random
 from reverse_polish_notation import *
 from monte_carlo import approximate_pi
-import simply_draw as sd
+import simple_draw as sd
 from l_system import draw
 def main():
     #print(rvp("((2+7)/3+(8-3)*4)/2")) # 2 7 + 3 / 8 3 − 4 * + 2 /
--- lab4/zad1/mainwidget.py
+++ lab4/zad1/mainwidget.py
@@ -1,15 +1,26 @@
-from PySide2.QtWidgets import QWidget, QPushButton
+from PySide2.QtWidgets import QWidget, QPushButton, QLineEdit,\
+    QLabel, QVBoxLayout, QMessageBox
+
 
 class MainWidget(QWidget):
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle("Main widget")
 
-        button = QPushButton("Press me", self)
+
+        button = QPushButton("Press me!", self)
         button.clicked.connect(self.onButtonClicked)
 
+
+        self. edit = QLineEdit(self)
+
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.edit)
+        layout.addWidget(button)
+
     def onButtonClicked(self):
-        #print("Clicked!")
-         self.close()
+        editText = self.edit.text()
+        self.label.setText(editText)
+        QMessageBox.critical(self, "Info", editText)
 
