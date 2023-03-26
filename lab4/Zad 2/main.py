37f04e18e60300db6b793800d84c95a0a3b78dd4
EPILZAK <edytapilzak40@gmail.com>
2023-03-26 12:09:41 +0200

    zad 2 kr 2

---
+++ lab4/Zad 2/main.py
@@ -0,0 +1,16 @@
+from PySide2.QtWidgets import QApplication, QWidget, QPushButton
+from mainwindow import MainWindow
+
+def main():
+    app = QApplication([])
+
+    window = MainWindow()
+    window.show()
+
+
+    app.exec_()
+
+
+
+if __name__ == "__main__":
+    main()
