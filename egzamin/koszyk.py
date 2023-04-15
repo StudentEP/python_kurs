9cbaee2ef2c3ee9b2cc2abb7be410e6d0d074572
EPILZAK <edytapilzak40@gmail.com>
2023-04-15 12:13:35 +0200

    krok 5

--- egzamin/koszyk.py
+++ egzamin/koszyk.py
@@ -1,8 +1,9 @@
-class Koszyk
+class Koszyk:
     def __init__(self):
         self.__produkty = {}
-    def dodaj(self. produkt):
-        if produkt in self.__produkty.keys():
+
+    def dodaj(self, produkt):
+        if produkt.get_nazwa() in self.__produkty.keys():
             self.__produkty[produkt.get_nazwa()] = produkt,self.__produkty[produkt.get_nazwa()][1]+1
         else:
-             self.__produkty[produkt.get_nazwa()] = produkt, 1
+            self.__produkty[produkt.get_nazwa()] = produkt, 1
